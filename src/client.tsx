8ba6ecc56e29b3b6fb0695703078f2e7ddcea242
y-yanagii <yyanagisawa555@gmail.com>
2021-07-26 19:59:54 +0900

    開発環境作成し直し、client側構築(#7)

--- src/client.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import CountUp from './components/CountUp';
-
-// エントリポイントの作成
-// SSRでないReactアプリはrender()を使用してUIを描画するが、SSRを使用してサーバ
-// で描画されている場合はhydrate()を使用し、サーバで描画した部分をブラウザで再描画しないようにします。
-// idがappの部分をhydrateで描画する
-hydrate(<CountUp />, document.querySelector('#app'));
