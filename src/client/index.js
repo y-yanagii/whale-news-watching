48284af8d4c8bc29fc0987d1628eaf4392888fa0
y-yanagii <yyanagisawa555@gmail.com>
2021-07-27 00:55:47 +0900

    eslint導入(#7)

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-fetch('/api/').then(response => {
+fetch(    '/api/'      ).then(response => {
   console.log(response);
 });
 
