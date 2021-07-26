a089d3a886f05c81f8888e8285986e072a436163
y-yanagii <yyanagisawa555@gmail.com>
2021-07-27 02:12:50 +0900

    test(chai sinon enzyme)の導入(#7)

---
+++ test/client/app.spec.js
@@ -0,0 +1,11 @@
+import { App } from '../../src/client/App';
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+describe('react test sample', () => {
+  it('rendering <div>Hello React !!!</div>', () => {
+    const result = shallow(<App />).contains(<div>Hello React !!!</div>);
+    expect(result).to.be.true
+  });
+});
---
+++ test/enzyme.js
@@ -0,0 +1,31 @@
+// test用のsetupスクリプト
+// テスト時にはDOMが存在しないため、globalにjsdomで作ったwindowオブジェクトを差し込んでいる
+// また同時に、enzymeの設定もここで行う
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.requestAnimationFrame = function(callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function(id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
+
+const Adapter = require('enzyme-adapter-react-16');
+
+require('enzyme').configure({adapter: new Adapter()});
