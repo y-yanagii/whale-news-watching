abcd27d6850d5d142f2526887553e4f3d7f4e5bf
y-yanagii <yyanagisawa555@gmail.com>
2021-07-28 14:48:15 +0900

    初期表示画面のレイアウト作成(#2

--- src/client/templates/Start.jsx
+++ src/client/templates/Start.jsx
@@ -4,8 +4,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Whale from '../assets/img/src/whale-sm.jpg';
+import Button from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  // 背景画像設定
   backImage: {
     height: 'calc(100vh)',
     backgroundImage: `url(${Whale})`,
@@ -13,9 +16,46 @@ const useStyles = makeStyles({
     backgroundRepeat: 'no-repeat',
     backgroundAttachment: 'fixed',
     backgroundSize: 'cover',
-    backgroundColor: '#464646'
-  }
-});
+    backgroundColor: '#284377'
+  },
+  catchphrase: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100%',
+    textAlign: 'center',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '3rem'
+    },
+    color: '#f3f9f9'
+  },
+  catchphraseH1: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '1.4rem',
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '2.5rem',
+    },
+    marginTop: 0,
+    marginBottom: '0.5rem',
+    fontFamily: 'sans-serif',
+    fontWeight: 500,
+    lineHeight: 1.2,
+    color: 'inherit'
+  },
+  button: {
+    [theme.breakpoints.down('md')]: {
+      width: '70%'
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '30%'
+    },
+    color: '#fff',
+    margin: theme.spacing(1),
+    fontFamily: 'initial',
+  },
+}));
 
 const Start = () => {
   const classes = useStyles();
@@ -25,7 +65,19 @@ const Start = () => {
       <CssBaseline />
       <Container maxWidth="xl" className={classes.backImage}>
         <Typography component="div" >
-          
+          <div className={classes.catchphrase}>
+            <h1 className={classes.catchphraseH1}>ネットという大海原で</h1>
+            <h1 className={classes.catchphraseH1}>ホエールウォッチングをしましょう</h1>
+            <div className="module-spacer--medium" />
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              endIcon={<SearchIcon />}
+            >
+              Whale Watching Start
+            </Button>
+          </div>
         </Typography>
       </Container>
     </>
