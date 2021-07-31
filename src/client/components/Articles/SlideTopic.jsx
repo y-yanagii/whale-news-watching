823f97bd4d987ab379886822485be6104af562ee
y-yanagii <yyanagisawa555@gmail.com>
2021-08-01 01:14:31 +0900

    トピックスをスライドで表示(#15)

--- src/client/components/Articles/SlideTopic.jsx
+++ src/client/components/Articles/SlideTopic.jsx
@@ -1,11 +1,72 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Carousel from "react-material-ui-carousel";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
 
+const useStyles = makeStyles({
+  media: {
+    height: 420,
+    backgroundColor: "#fff",
+    overflow: "hidden",
+    position: "relative",
+    transition: ".3s",
+    cursor: "pointer"
+  },
+  title: {
+    textOverflow: "ellipsis",
+    position: "absolute",
+    margin: 0,
+    bottom: 0,
+    padding: "15px",
+    backgroundColor: "#000",
+    color: "#fff",
+    opacity: ".6",
+    width: "95%",
+    height: "20%",
+    fontSize: "25px",
+    fontWeight: 200,
+    transition: ".3s",
+    cursor: "pointer"
+  }
+});
+
+// トピックスのスライド領域コンポーネント
 const SlideTopic = (props) => {
+  const classes = useStyles();
+
+  const articles = props.slideArticles;
+
   return (
     <>
-      <div>Slide</div>
+      <Carousel
+        autoPlay={true}
+      >
+        {articles.map((article, i) => (
+          <Paper key={i}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={article.urlToImage}
+                  title={article.title}
+                />
+                <p className={classes.title}>{article.title}</p>
+              </CardActionArea>
+            </Card>
+          </Paper>
+        ))}
+      </Carousel>
     </>
   )
 }
+
+// propsの型チェック
+SlideTopic.propTypes = {
+  slideArticles: PropTypes.array
+};
 
 export default SlideTopic;
--- src/client/components/Articles/Topics.jsx
+++ src/client/components/Articles/Topics.jsx
@@ -1,17 +1,47 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import PropTypes from "prop-types";
 import { SlideTopic, SubTopic } from "./index";
 
+const useStyles = makeStyles((theme) => ({
+  // 親領域
+  topicsContainer: {
+    position: "relative",
+    height: "100%"
+  },
+  // スライド領域
+  slide: {
+    position: "relative",
+    top: 0,
+    width: "66.64%",
+    height: "100%"
+  },
+  // サブトピックス領域
+  sub: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: "33.26%",
+    height: "100%",
+    display: "inline-block"
+  }
+}));
+
 const Topics = (props) => {
-  console.log(props.slideArticles);
-  console.log(props.subTopicArticles);
+  const classes = useStyles();
 
   return (
     <Container maxWidth="md">
-      <SlideTopic />
-      <SubTopic />
-      <SubTopic />
+      <div className={classes.topicsContainer}>
+        <div className={classes.slide}>
+          <SlideTopic slideArticles={props.slideArticles} />
+        </div>
+        <div className={classes.sub}>
+          <SubTopic />
+          <SubTopic />
+        </div>
+      </div>
     </Container>
   )
 };
