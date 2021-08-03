d447fa35e30f5ffb6be36bb6cf1d5cf5fdf19f05
y-yanagii <yyanagisawa555@gmail.com>
2021-08-03 14:16:30 +0900

    ヘッダー領域作成

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/styles";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/ToolBar";
 import logo from "../../assets/img/icons/logo.png";
+import { HeaderMenus } from "./index";
 
 const useStyles = makeStyles({
   root: {
@@ -16,23 +17,35 @@ const useStyles = makeStyles({
     borderRadius: "6px"
   },
   toolBar: {
-    margin: "0 auto",
-    maxWidth: 1024,
     width: "100%",
+    "& > img": {
+      margin: "0 auto 0 0",
+      cursor: "pointer"
+    }
   },
+  iconButtons: {
+    margin: "0 3% 0 auto"
+  }
 });
 
 const Header = () => {
   const classes = useStyles();
+
+  const isSignedIn = true;
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed" className={classes.menuBar}>
         <ToolBar className={classes.toolBar}>
           <img
-            src={logo} alt="Whale Watching Start" width="128px"
+            src={logo} alt="Whale News Watching" width="128px"
             onClick={() => console.log("ホーム画面へ")}
           />
+          {isSignedIn && (
+            <div className={classes.iconButtons}>
+              <HeaderMenus />
+            </div>
+          )}
         </ToolBar>
       </AppBar>
     </div>
