c1fc9010ba52a56687716a85127703452c40f305
y-yanagii <yyanagisawa555@gmail.com>
2021-08-05 01:53:33 +0900

    api処理部分をフォルダに切り分け(#29)

---
+++ src/server/api/articles.js
@@ -0,0 +1,18 @@
+import pool from "../postgresql"; // postgresqlの設定ファイル
+let router = require("express").Router();
+
+router.get("/*", (req, res) => {
+  // 記事情報一覧取得(DB接続)
+  pool.connect(function(err, client) {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query("SELECT * FROM articles", function (err, result) {
+        console.log(result.rows);
+        res.json(result.rows);
+      });
+    }
+  });
+});
+
+module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ import path from "path";
 import config from "config";
 import { logger } from "./logger";
 import NewsAPI from "newsapi";
-import pool from "./postgresql"; // postgresqlの設定ファイル
+// import pool from "./postgresql"; // postgresqlの設定ファイル
 import jsonData from "../../sample.json";
 
 const app = express();
@@ -14,42 +14,49 @@ const serverConfig = config.get("server");
 
 app.use(express.static(path.resolve("./", "dist")));
 
-app.get("/api", (req, res) => {
-  // 記事情報一覧取得(DB接続)
-  pool.connect(function(err, client) {
-    if (err) {
-      console.log(err);
-    } else {
-      client.query("SELECT * FROM articles", function (err, result) {
-        console.log("query結果 --------------------");
-        console.log(result);
-        res.json(result.rows);
-      });
-    }
-  });
+// api用のrouter読み込み(即時関数)
+app.use("/api", (() => {
+  let router = express.Router();
+  router.use("/articles", require("./api/articles.js")); // 記事情報関連API
+  return router;
+})());
 
-  // サンプル
-  // res.json(jsonData);
-});
+// api用のrouter読み込み
+// app.get("/api", (req, res) => {
+//   // 記事情報一覧取得(DB接続)
+//   pool.connect(function(err, client) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       client.query("SELECT * FROM articles", function (err, result) {
+//         console.log(result.rows);
+//         res.json(result.rows);
+//       });
+//     }
+//   });
+
+//   // サンプル
+//   // res.json(jsonData);
+// });
 
 // 記事一覧初期表示
-app.get("/api/articles", async (req, res) => {
-  const newsapi = new NewsAPI("94694ec2cdbf458d824851f951c3dd3c");
-  try {
-    await newsapi.v2.everything({
-      q: "クジラ",
-      categroy: "jp"
-    }).then(response => {
-      // console.log(response.articles);
-      res.json({ articles: response.articles});
-    }).catch(error => {
-      throw error;
-    });
-  } catch(error) {
-    console.log(error);
-    throw error;
-  }
-});
+// app.get("/api/articles", async (req, res) => {
+//   const newsapi = new NewsAPI("94694ec2cdbf458d824851f951c3dd3c");
+//   try {
+//     await newsapi.v2.everything({
+//       q: "クジラ",
+//       categroy: "jp"
+//     }).then(response => {
+//       // console.log(response.articles);
+//       res.json({ articles: response.articles});
+//     }).catch(error => {
+//       throw error;
+//     });
+//   } catch(error) {
+//     console.log(error);
+//     throw error;
+//   }
+// });
 
 app.get("*", function(req, res) {
   res.sendFile(path.resolve("./", "dist", "index.html"))
